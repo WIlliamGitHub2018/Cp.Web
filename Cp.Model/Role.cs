e9134ba93c5d7f4118f880055af5c5791a20603f
Williams <63750898@qq.com>
2018-08-08 14:39:38 +0800

    新增用户角色实体

---
+++ Cp.Model/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cp.Model
+{
+    public class Role:BaseEntity
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 角色类型
+        /// </summary>
+        public int RoleType { get; set; }
+    }
+}
--- Cp.Model/User.cs
+++ Cp.Model/User.cs
@@ -8,20 +8,41 @@ namespace Cp.Model
 {
     public class User:BaseEntity
     {
+        /// <summary>
+        /// 账号
+        /// </summary>
         public string Account { get; set; }
-
+        /// <summary>
+        /// 密码
+        /// </summary>
         public string Pwd { get; set; }
-
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// 电话
+        /// </summary>
         public string Phone { get; set; }
-
+        /// <summary>
+        /// 身份证号
+        /// </summary>
         public string IDCard { get; set; }
-
-        public string Sex { get; set; }
-
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int Sex { get; set; }
+        /// <summary>
+        /// 职位
+        /// </summary>
         public string Post { get; set; }
-
+        /// <summary>
+        /// 邮箱
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
     }
 }
